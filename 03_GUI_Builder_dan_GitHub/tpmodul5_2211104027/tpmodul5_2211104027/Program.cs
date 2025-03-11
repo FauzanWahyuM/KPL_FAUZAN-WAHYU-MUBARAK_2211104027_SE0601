63b4311287b6842e885700e31d730f131dba02ac
Fauzan Wahyu Mubarak <114811887+FauzanWahyuM@users.noreply.github.com>
2025-03-11 22:26:22 +0700

    Praktikum 3 (TP Modul 5) : Membuat class HaloGeneric dengan method SapaUser dan menambahkan class DataGeneric

---
+++ 03_GUI_Builder_dan_GitHub/tpmodul5_2211104027/tpmodul5_2211104027/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_2211104027
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Memanggil metode generic SapaUser
+            HaloGeneric.SapaUser("Fauzan");
+
+            // Menggunakan kelas DataGeneric
+            DataGeneric<string> dataGeneric = new DataGeneric<string>("NIM : 2211104027");
+            dataGeneric.PrintData();
+        }
+    }
+
+    // HaloGeneric.cs
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+
+    // DataGeneric.cs
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah {data}");
+        }
+    }
+
+}
